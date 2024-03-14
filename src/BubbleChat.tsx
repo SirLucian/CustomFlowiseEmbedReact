2c0d216dc53123f8c31d95567322aad360ab2b9b
SirLucian <lucian.blankevoort@gmail.com>
2024-03-14 07:25:12 +0200

    new build

--- src/BubbleChat.tsx
+++ src/BubbleChat.tsx
@@ -1,53 +1,53 @@
-import { useCallback, useEffect, useRef, useState } from 'react'
-import type { BubbleProps } from 'flowise-embed'
+import { useCallback, useEffect, useRef, useState } from "react";
+import type { BubbleProps } from "chatasaurus-embed";
 
-type Props = BubbleProps
+type Props = BubbleProps;
 
 declare global {
   namespace JSX {
     interface IntrinsicElements {
-      'flowise-chatbot': React.DetailedHTMLProps<
+      "flowise-chatbot": React.DetailedHTMLProps<
         React.HTMLAttributes<HTMLElement>,
         HTMLElement
-      >
+      >;
     }
   }
 }
 
-type BubbleElement = HTMLElement & Props
+type BubbleElement = HTMLElement & Props;
 
 export const BubbleChat = (props: Props) => {
-  const ref = useRef<BubbleElement | null>(null)
-  const [isInitialized, setIsInitialized] = useState(false)
+  const ref = useRef<BubbleElement | null>(null);
+  const [isInitialized, setIsInitialized] = useState(false);
 
   useEffect(() => {
-    ;(async () => {
-      await import('flowise-embed/dist/web')
-      setIsInitialized(true)
-    })()
+    (async () => {
+      await import("chatasaurus-embed/dist/web");
+      setIsInitialized(true);
+    })();
     return () => {
-      ref.current?.remove()
-    }
-  }, [])
+      ref.current?.remove();
+    };
+  }, []);
 
   const attachBubbleToDom = useCallback((props: Props) => {
     const bubbleElement = document.createElement(
-      'flowise-chatbot'
-    ) as BubbleElement
-    ref.current = bubbleElement
-    injectPropsToElement(ref.current, props)
-    document.body.append(ref.current)
-  }, [])
+      "flowise-chatbot"
+    ) as BubbleElement;
+    ref.current = bubbleElement;
+    injectPropsToElement(ref.current, props);
+    document.body.append(ref.current);
+  }, []);
 
   useEffect(() => {
-    if (!isInitialized) return
-    if (!ref.current) attachBubbleToDom(props)
-    injectPropsToElement(ref.current as BubbleElement, props)
-  }, [attachBubbleToDom, isInitialized, props])
+    if (!isInitialized) return;
+    if (!ref.current) attachBubbleToDom(props);
+    injectPropsToElement(ref.current as BubbleElement, props);
+  }, [attachBubbleToDom, isInitialized, props]);
 
   const injectPropsToElement = (element: BubbleElement, props: Props) => {
-    Object.assign(element, props)
-  }
+    Object.assign(element, props);
+  };
 
-  return null
-}
+  return null;
+};
--- src/FullPageChat.tsx
+++ src/FullPageChat.tsx
@@ -1,37 +1,41 @@
-import { useEffect, useRef } from 'react'
-import type { BotProps } from 'flowise-embed'
+import { useEffect, useRef } from "react";
+import type { BotProps } from "chatasaurus-embed";
 
 type Props = BotProps & {
-  style?: React.CSSProperties
-  className?: string
-}
+  style?: React.CSSProperties;
+  className?: string;
+};
 
 declare global {
   namespace JSX {
     interface IntrinsicElements {
-      'flowise-fullchatbot': React.DetailedHTMLProps<
+      "flowise-fullchatbot": React.DetailedHTMLProps<
         React.HTMLAttributes<HTMLElement>,
         HTMLElement
-      > & { class?: string }
+      > & { class?: string };
     }
   }
 }
 
-type FullPageChatElement = HTMLElement & Props
+type FullPageChatElement = HTMLElement & Props;
 
-export const FullPageChat = ({ style, className, ...assignableProps }: Props) => {
-  const ref = useRef<FullPageChatElement | null>(null)
-
-  useEffect(() => {
-    ;(async () => {
-      await import('flowise-embed/dist/web')
-    })()
-  }, [])
+export const FullPageChat = ({
+  style,
+  className,
+  ...assignableProps
+}: Props) => {
+  const ref = useRef<FullPageChatElement | null>(null);
 
   useEffect(() => {
-    if (!ref.current) return
-    Object.assign(ref.current, assignableProps)
-  }, [assignableProps])
+    (async () => {
+      await import("chatasaurus-embed/dist/web");
+    })();
+  }, []);
 
-  return <flowise-fullchatbot ref={ref} style={style} class={className} />
-}
+  useEffect(() => {
+    if (!ref.current) return;
+    Object.assign(ref.current, assignableProps);
+  }, [assignableProps]);
+
+  return <flowise-fullchatbot ref={ref} style={style} class={className} />;
+};
